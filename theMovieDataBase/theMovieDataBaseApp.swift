4578514c3ed3297c871bf7803ede8936cd274252
Balint Lakatos <lakatosbalint911@gmail.com>
2023-05-16 17:55:54 +0200

    Initial Commit

---
+++ theMovieDataBase/theMovieDataBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  theMovieDataBaseApp.swift
+//  theMovieDataBase
+//
+//  Created by Bálna on 2023. 05. 16..
+//
+
+import SwiftUI
+
+@main
+struct theMovieDataBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
